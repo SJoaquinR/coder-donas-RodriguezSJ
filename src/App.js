41319458d6840f447bd439c0ecde55c3c054b1b4
SJoaquinR <rsanjoa@gmail.com>
2021-10-01 15:55:00 -0300

    codigo inicial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>bienvenido al local de Donas!</h1>
+     <img src="logo512.png" alt="Imagen de donas"></img>
+    </div>
+  );
+}
+
+export default App;
